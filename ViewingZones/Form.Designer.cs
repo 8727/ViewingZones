082815353a97e4ebca0ddfbd3c924cfd1962207d
alexli2024@outlook.com <alexli2024@outlook.com>
2024-05-07 19:04:09 +0300

    Добавьте файлы проекта.

---
+++ ViewingZones/Form.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ViewingZones
+{
+    partial class Ui
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ui));
+            this.searchNumber = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.Button();
+            this.searchNumber.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchNumber
+            // 
+            this.searchNumber.Controls.Add(this.search);
+            this.searchNumber.Controls.Add(this.textBox1);
+            this.searchNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchNumber.Location = new System.Drawing.Point(10, 2);
+            this.searchNumber.Name = "searchNumber";
+            this.searchNumber.Size = new System.Drawing.Size(466, 79);
+            this.searchNumber.TabIndex = 0;
+            this.searchNumber.TabStop = false;
+            this.searchNumber.Text = "Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "А001АА177";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(332, 28);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(97, 37);
+            this.search.TabIndex = 1;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1457, 808);
+            this.Controls.Add(this.searchNumber);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "Form";
+            this.searchNumber.ResumeLayout(false);
+            this.searchNumber.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox searchNumber;
+        private System.Windows.Forms.Button search;
+    }
+}
+
